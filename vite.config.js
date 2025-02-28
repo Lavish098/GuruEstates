e7b0ca407e5a8f184dd883adac7829b2d4a37a4f
Lavish098 <75539589+Lavish098@users.noreply.github.com>
2025-02-28 06:46:54 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
+  base: "/", // Ensures correct base path for Vercel
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: true,
+  },
 });
