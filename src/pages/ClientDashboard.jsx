f21fa27d1f74febb87c02bf264bafc9cde3f78ec
Lavish098 <75539589+Lavish098@users.noreply.github.com>
2025-05-02 17:28:38 +0100

    Update ClientDashboard.jsx

--- src/pages/ClientDashboard.jsx
+++ src/pages/ClientDashboard.jsx
@@ -58,7 +58,7 @@ const ClientDashboard = () => {
               </Button>
             </Link>
           </div>
-          <div className="">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             <PropertiesList isHome={true} />
           </div>
         </div>
